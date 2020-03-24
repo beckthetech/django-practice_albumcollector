c284f146b839809512b8f7f8bcac0aa0314b2861
Daniel Beck <dlc.beck@gmail.com>
2020-03-23 18:21:21 -0700

    update

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.db.models import IntegerField, Model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
